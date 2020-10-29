a34f90fd778d6fde9c30c58a7cd489ad97219d07
Mae Kindler <mae.akie.kindler@gmail.com>
2020-10-29 11:46:13 -0500

    Added a constant variable to create the current day and date in the jumbotron that gets updated with the day's information. Also added the function for the save button and created variables for grabbing the values needed for displaying what will be saved in the time blocks provided. Finally, set up local storage so data will be saved in local storage as it is written to the page.

---
+++ script.js
@@ -0,0 +1,14 @@
+const newLocal = $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+$(document).ready(function() {
+    // listen for save button clicks
+    $(".saveBtn").on("click", function() {
+      // get nearby values
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+  
+      // save in localStorage
+      localStorage.setItem(time, value);
+    });
+  
+    
