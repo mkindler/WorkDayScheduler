a5edbf9204765171e5022b3b2e126df4932cb912
Mae Kindler <mae.akie.kindler@gmail.com>
2020-10-29 17:18:49 -0500

    Attempted another method for adding data to each hour block and saving the data to local storage. Additionally, using this method to pull the data saved in local storage when the page is reloaded. This attempt did not work.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let currentDate = $("#currentDay").text(moment().format("dddd, MMMM Do"));
 // Retrieve the current number of hours
 let currentHour = moment().hours();
 
+let toDoItems = [];
+
 $(document).ready(function() {
 
     // Format each block of time with colors based on the current time of day.  For example, hour blocks that are in the past will be shaded light grey, the current/present hour block will be shaded red, and the future hour blocks will be shaded green.
@@ -25,29 +27,54 @@ $(document).ready(function() {
                 $(this).removeClass("present");                
                 $(this).addClass("future");                
             }
+
         });
     }
 
     // Run the function to format the hour blocks.
     updateHourBlocks();
 
+    function updateToDo() {
+        $(".time-block").each(function() {
+            let toDoObject = {
+                hour: hourBlock,
+                data: "",
+            }
+
+            toDoItems.push(toDoObject);
+        });
+        localStorage.setItem("todo", JSON.stringify(toDoItems));
+    }
+
+    updateToDo();
+
+    function showSchedule() {
+        toDoItems = localStorage.getItem("todo");
+        toDoItems = JSON.parse(toDoItems);
+
+        for (let i = 0; i < toDoItems.length; i++) {
+            let itemHour = toDoItems[i].hour;
+            let itemData = toDoItems[i].data;
+
+            $("[hour=" + itemHour + "]").children("textarea").val(itemData);
+        }
+    }
+
     // When a user clicks the save button, the computer will listen for this action, obtain the data placed in each text area box based on the hour block, and will then save this data to localStorage.
     $(".saveBtn").on("click", function() {
         let toDo = $(this).siblings(".toDo").val();
         let time = $(this).parent().attr("hour");
 
-        localStorage.setItem(time, toDo);
+        for (let j = 0; j < toDoItems.length; j++) {
+            if (toDoItems[j].hour === time) {
+                toDoItems[j].data === toDo;
+            }
+        }
+        localStorage.setItem("todo", JSON.stringify(toDoItems));
+        showSchedule();
     });
 
     // When the scheduler is pulled up on a browser, the following will load any data that has been stored in localStorage.
-    $("hour, .toDo").val(localStorage.getItem("9"));
-    // $("hour, .toDo").val(localStorage.getItem("10"));
-    // $("hour, .toDo").val(localStorage.getItem("11"));
-    // $("hour, .toDo").val(localStorage.getItem("12"));
-    // $("hour, .toDo").val(localStorage.getItem("13"));
-    // $("hour, .toDo").val(localStorage.getItem("14"));
-    // $("hour, .toDo").val(localStorage.getItem("15"));
-    // $("hour, .toDo").val(localStorage.getItem("16"));
-    // $("hour, .toDo").val(localStorage.getItem("17"));
+    
 
 });
